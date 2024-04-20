ef4b629dc18ecfb02db3a52709942f4fba71e3e7
swaminathan23 <161034085+swaminathan23@users.noreply.github.com>
2024-04-21 00:13:22 +0530

    Update Suduko.java

--- suduko/src/suduko/Suduko.java
+++ suduko/src/suduko/Suduko.java
@@ -3,8 +3,8 @@ package suduko;
 import java.util.Scanner;
 
 public class Suduko {
-	public static int gridsize=3;
-	private static int size=9;
+	public static int gridsize;
+	private static int size;
 	
 	private static boolean checkRow(int[][] mat,int row,int num) {
 		for(int i=0;i<mat.length;i++) {
@@ -30,7 +30,15 @@ public class Suduko {
 		return false;
 	}
 	private static boolean validPlace(int[][] mat,int col,int row,int num) {
+		//System.out.println("valid place ");
 		if(!checkGrid(mat,col,row,num)&&!checkCol(mat,col,num)&&!checkRow(mat,row,num)) {
+			return true;
+		}
+		return false;
+	}
+	private static boolean  validPlace1(int[][] mat,int col,int row,int num) {
+		//System.out.println("valid place 1");
+		if(!checkCol(mat,col,num)&&!checkRow(mat,row,num)) {
 			return true;
 		}
 		return false;
@@ -41,7 +49,6 @@ public class Suduko {
 				System.out.print(mat[i][j]+"|");
 			}
 			System.out.println();
-			//if(size%gridsize==i)System.out.println("---------");
 		}
 	}
 	private static boolean  solve(int[][]mat) {
@@ -49,12 +56,19 @@ public class Suduko {
 			for(int j=0;j<mat.length;j++) {
 			if(mat[i][j]==0) {
 				for(int k=1;k<=size;k++) {
-					if(validPlace(mat,j,i,k)) {
+					if(gridsize!=0&&validPlace(mat,j,i,k)) {
 						mat[i][j]=k;
 						if(solve(mat)) {
 							return true;
 						}
-						else mat[i][j]=0;
+						else {mat[i][j]=0;}
+					}
+					else if(gridsize==0&&validPlace1(mat,j,i,k)) {
+						mat[i][j]=k;
+						if(solve(mat)) {
+							return true;
+						}
+						else {mat[i][j]=0;}
 					}
 				}
 				return false;
@@ -64,36 +78,28 @@ public class Suduko {
 		return true;
 	}
 	public static void main(String[] args) {
-//		System.out.println("enter the no of colum");
-//		Scanner s=new Scanner(System.in);
-//		size=s.nextInt();
-//		for(int i=0;i<size;i++)if(i*i==size) {gridsize=i;break;}
-//		int[][] mat=new int[size][size];
-//		while(true) {
-//			System.out.println("enter 1 for partial input or anyother  to solve the suduko");
-//			int a=s.nextInt();
-//			if(a==1) {
-//				System.out.println("enter initial input\n enter the rownumber");
-//				int row=s.nextInt();
-//				System.out.println("enter the col number");
-//				int col=s.nextInt();
-//				System.out.println("enter the input");
-//				int input=s.nextInt();
-//				mat[row][col]=input;
-//			}
-//			else {printSuduko(mat);
-//				break;}
-//			
-//		}
-		int [][]mat= {{1,2,0,3,0,4,0,5,6},
-					{7,0,0,0,0,6,0,0,1},
-					{0,0,0,0,0,0,0,0,0},
-					{0,8,0,4,0,9,0,2,0},
-					{0,0,0,0,6,0,0,0,0},
-					{0,3,0,5,0,1,0,8,0},
-					{0,0,0,0,0,0,0,0,0},
-					{9,0,0,2,0,0,0,0,8},
-					{8,4,0,6,0,7,0,1,9}};
+		System.out.println("enter the no of colum ");
+		Scanner s=new Scanner(System.in);
+		size=s.nextInt();
+		for(int i=0;i<size;i++)if(i*i==size) {gridsize=i;break;}
+		int[][] mat=new int[size][size];
+		while(true) {
+			System.out.println("enter 1 for partial input or anyother  to solve the suduko");
+			int a=s.nextInt();
+			if(a==1) {
+				System.out.println("enter initial input\n enter the rownumber");
+				int row=s.nextInt();
+				System.out.println("enter the col number");
+				int col=s.nextInt();
+				System.out.println("enter the input");
+				int input=s.nextInt();
+				mat[row][col]=input;
+			}
+			else {printSuduko(mat);
+				break;}
+			
+		}
+		//int [][]mat= {{0,0,3,0},{0,0,1,0},{0,0,0,1},{3,0,2,0}};
 		
 		if(solve(mat)) {
 			System.out.println("suduko solved");
